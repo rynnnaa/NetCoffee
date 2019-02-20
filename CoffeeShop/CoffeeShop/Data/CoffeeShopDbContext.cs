acf38fc8a22b34542ef5fd8fa6019ed28fcdace0
Tulip Pink <pinktulips@hotmail.com>
2019-02-19 21:19:27 -0800

    added database

---
+++ CoffeeShop/CoffeeShop/Data/CoffeeShopDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Data
+{
+    public class CoffeeShopDbContext : DbContext
+    {
+        public CoffeeShopDbContext(DbContextOptions<CoffeeShopDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+    }
+}
