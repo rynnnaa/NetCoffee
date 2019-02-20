725f5d52101e831c1181f682f956750a1a33b3ae
Tulip Pink <pinktulips@hotmail.com>
2019-02-20 11:44:25 -0800

    Register View Model Properties

--- CoffeeShop/CoffeeShop/Models/ViewModels/RegisterViewModel.cs
+++ CoffeeShop/CoffeeShop/Models/ViewModels/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,25 @@ namespace CoffeeShop.Models.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation do not match")]
+        public string ConfirmPassword { get; set; }
     }
 }
