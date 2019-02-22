1a240e6d93907fbeecd602835823fc844301327c
Tulip Pink <pinktulips@hotmail.com>
2019-02-22 11:15:56 -0800

    policy controller

--- CoffeeShop/CoffeeShop/Controllers/PolicyController.cs
+++ CoffeeShop/CoffeeShop/Controllers/PolicyController.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
+
 namespace CoffeeShop.Controllers
 {
-    public class PolicyController
+    [Authorize(Policy = "Seattleite")]
+    public class PolicyController : Controller
     {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Seattlelite()
+        {
+            return View();
+        }
     }
 }
