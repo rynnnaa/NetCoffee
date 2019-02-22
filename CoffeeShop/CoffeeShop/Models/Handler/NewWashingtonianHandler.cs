3d11fd7d744baf5bc72fcdf449ffa54197113387
Tulip Pink <pinktulips@hotmail.com>
2019-02-22 13:11:39 -0800

    handler requirements

---
+++ CoffeeShop/CoffeeShop/Models/Handler/NewWashingtonianHandler.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models.Handler
+{
+    public class NewWashingtonianHandler : AuthorizationHandler<WashingtonianRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, WashingtonianRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == ClaimTypes.StateOrProvince))
+            {
+                return Task.CompletedTask();
+            }
+
+            var washington = context.User.FindFirst(c => c.Type == ClaimTypes.StateOrProvince).Value;
+
+
+
+            if (washington != "Washington")
+            {
+
+            }
+            if (washington == "Washington")
+            {
+                context.Succeed(requirement);
+            }
+
+
+
+            return Task.CompletedTask;
+        }
+
+
+    }
+}
---
+++ CoffeeShop/CoffeeShop/Models/Handler/WashingtonianRequirement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CoffeeShop.Models.Handler
+{
+    public class WashingtonianRequirement : Microsoft.AspNetCore.Authorization.AuthorizationHandler<WashingtonianRequirement>, IAuthorizationRequirement
+    {
+        private string _state;
+
+        public WashingtonianRequirement(string state)
+        {
+            _state = state;
+        }
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, WashingtonianRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == ClaimTypes.StateOrProvince))
+            {
+                return Task.CompletedTask();
+            }
+
+            var washington = context.User.FindFirst(c => c.Type == ClaimTypes.StateOrProvince).Value;
+
+
+
+            if (washington != "Washington")
+            {
+
+            }
+            if (washington == "Washington")
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+
+    }
+}
