903bde8f0739ae6ea13f4fbef28ef7f0bb992820
rjjohnson1204 <rjjohnson1204@gmail.com>
2019-03-04 10:54:34 -0800

    added Iconfiguration

---
+++ CoffeeShop/CoffeeShop/Models/Payment.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public class Payment
+    {
+        private IConfiguration _configuration;
+        public Payment(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+    }
+}
