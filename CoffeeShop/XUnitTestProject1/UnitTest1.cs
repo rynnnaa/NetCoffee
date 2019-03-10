b1873585a8f537c72cd61f6b8b26f6118442c360
Tulip Pink <pinktulips@hotmail.com>
2019-03-10 15:58:11 -0700

    getters and setters for cofffee

--- CoffeeShop/XUnitTestProject1/UnitTest1.cs
+++ CoffeeShop/XUnitTestProject1/UnitTest1.cs
@@ -7,7 +7,26 @@ namespace XUnitTestProject1
     public class UnitTest1
     {
         [Fact]
-        public void CanGetCoffee()
+        public void CanGetCoffeeID()
+        {
+            Coffee coffee = new Coffee();
+            coffee.ID = 1;
+
+            Assert.Equal(1, coffee.ID);
+        }
+
+        [Fact]
+        public void CanSetCoffeeID()
+        {
+            Coffee coffee = new Coffee();
+            coffee.ID = 2;
+            coffee.ID = 1;
+
+            Assert.Equal(1, coffee.ID);
+        }
+
+        [Fact]
+        public void CanGetCoffeeName()
         {
             Coffee coffee = new Coffee();
             coffee.Name = "Latte";
@@ -16,7 +35,7 @@ namespace XUnitTestProject1
         }
 
         [Fact]
-        public void CanSetCoffee()
+        public void CanSetCoffeeName()
         {
             Coffee coffee = new Coffee();
             coffee.Name = "Espresso";
@@ -24,5 +43,72 @@ namespace XUnitTestProject1
 
             Assert.Equal("Latte", coffee.Name);
         }
+
+        [Fact]
+        public void CanGetCoffeePrice()
+        {
+            Coffee coffee = new Coffee();
+            coffee.Price = 1;
+
+            Assert.Equal(1, coffee.Price);
+        }
+
+        [Fact]
+        public void CanSetCoffeePrice()
+        {
+            Coffee coffee = new Coffee();
+            coffee.Price = 2;
+            coffee.Price = 1;
+
+            Assert.Equal(1, coffee.Price);
+        }
+
+        [Fact]
+        public void CanGetCoffeeURL()
+        {
+            Coffee coffee = new Coffee();
+            coffee.URL = "www.coffee.com";
+
+            Assert.Equal("www.coffee.com", coffee.URL);
+        }
+
+        [Fact]
+        public void CanSetCoffeeURL()
+        {
+            Coffee coffee = new Coffee();
+            coffee.URL = "www.notcoffee.com";
+            coffee.URL = "www.coffee.com";
+
+            Assert.Equal("www.coffee.com", coffee.URL);
+        }
+
+        [Fact]
+        public void CanGetCoffeeDescription()
+        {
+            Coffee coffee = new Coffee();
+            coffee.Description = "tasty";
+
+            Assert.Equal("tasty", coffee.Description);
+        }
+
+        [Fact]
+        public void CanSetCoffeeDescription()
+        {
+            Coffee coffee = new Coffee();
+            coffee.Description = "not tasty";
+            coffee.Description = "tasty";
+
+            Assert.Equal("tasty", coffee.Description);
+        }
+
+        [Fact]
+        public void CanGetOrder()
+        {
+            Coffee coffee = new Coffee();
+            coffee.Description = "tasty";
+
+            Assert.Equal("tasty", coffee.Description);
+        }
     }
+}
 }
