60f8aeca4c5286d69f05a291aa3cfa9849b400ee
Tulip Pink <pinktulips@hotmail.com>
2019-03-10 17:01:08 -0700

    create and delete

--- CoffeeShop/XUnitTestProject1/UnitTest1.cs
+++ CoffeeShop/XUnitTestProject1/UnitTest1.cs
@@ -1,5 +1,9 @@
+using CoffeeShop.Data;
 using CoffeeShop.Models;
+using CoffeeShop.Models.Services;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace XUnitTestProject1
@@ -39,7 +43,7 @@ namespace XUnitTestProject1
         {
             Coffee coffee = new Coffee();
             coffee.Name = "Espresso";
-            coffee.Name = "Latte";         
+            coffee.Name = "Latte";
 
             Assert.Equal("Latte", coffee.Name);
         }
@@ -177,32 +181,146 @@ namespace XUnitTestProject1
             Assert.Equal("luther", order.LastName);
         }
 
-        //[Fact]
-        //public void CanGetOrderLines()
-        //{
-        //    Order order = new Order();
-        //    order.OrderLines = ;
-
-        //    Assert.Equal("luther", order.OrderLines);
-        //}
-
-        //[Fact]
-        //public void CanSetOrderLines()
-        //{
-        //    Order order = new Order();
-        //    order.OrderLines = ;
-
-        //    Assert.Equal("luther", order.OrderLines);
-        //}
 
         [Fact]
-        public void CanGetOrderLines()
+        public void CanGetOrderDetailId()
         {
-            Order order = new Order();
-            order.OrderLines = ;
+            OrderDetail order = new OrderDetail();
+            order.OrderDetailId = 1;
 
-            Assert.Equal("luther", order.OrderLines);
+            Assert.Equal(1, order.OrderDetailId);
         }
 
+        [Fact]
+        public void CanSetOrderDetailId()
+        {
+            OrderDetail order = new OrderDetail();
+            order.OrderDetailId = 2;
+            order.OrderDetailId = 1;
+
+            Assert.Equal(1, order.OrderDetailId);
+        }
+
+        [Fact]
+        public void CanGetOrderDetailOrderId()
+        {
+            OrderDetail order = new OrderDetail();
+            order.OrderId = 1;
+
+            Assert.Equal(1, order.OrderId);
+        }
+
+        [Fact]
+        public void CanSetOrderDetailOrderId()
+        {
+            OrderDetail order = new OrderDetail();
+            order.OrderId = 2;
+            order.OrderId = 1;
+
+            Assert.Equal(1, order.OrderId);
+        }
+
+        [Fact]
+        public void CanGetOrderDetailCoffee()
+        {
+            OrderDetail order = new OrderDetail();
+            order.CoffeeId = 1;
+
+            Assert.Equal(1, order.CoffeeId);
+        }
+
+        [Fact]
+        public void CanSetOrderDetailCoffee()
+        {
+            OrderDetail order = new OrderDetail();
+            order.CoffeeId = 2;
+            order.CoffeeId = 1;
+
+            Assert.Equal(1, order.CoffeeId);
+        }
+
+        [Fact]
+        public void CanGetOrderDetailAmount()
+        {
+            OrderDetail order = new OrderDetail();
+            order.Amount = 1;
+
+            Assert.Equal(1, order.Amount);
+        }
+
+        [Fact]
+        public void CanSetOrderDetailAmount()
+        {
+            OrderDetail order = new OrderDetail();
+            order.Amount = 2;
+            order.Amount = 1;
+
+            Assert.Equal(1, order.Amount);
+        }
+
+
+        [Fact]
+        public void CanGetOrderDetailPrice()
+        {
+            OrderDetail order = new OrderDetail();
+            order.Price = 1;
+
+            Assert.Equal(1, order.Price);
+        }
+
+        [Fact]
+        public void CanSetOrderDetailPrice()
+        {
+            OrderDetail order = new OrderDetail();
+            order.Price = 2;
+            order.Price = 1;
+
+            Assert.Equal(1, order.Price);
+        }
+
+        [Fact]
+        public async void CanCreateCoffee()
+        {
+            DbContextOptions<CoffeeShopDbContext> options = new DbContextOptionsBuilder<CoffeeShopDbContext>().UseInMemoryDatabase("CanCreateCoffee").Options;
+            using (CoffeeShopDbContext context = new CoffeeShopDbContext(options))
+            {
+                Coffee coffee = new Coffee();
+                coffee.ID = 1;
+                coffee.Name = "latte";
+                coffee.Price = 2;
+                coffee.Description = "tasty";
+                coffee.URL = "www.coffee.com";
+
+                CoffeeManager coffeeManager = new CoffeeManager(context);
+
+                await coffeeManager.CreateCoffee(coffee);
+
+                var result = context.Coffee.FirstOrDefault(c => c.ID == coffee.ID);
+
+                Assert.Equal(coffee, result);
+            }
+        }
+        [Fact]
+        public async void CanDeleteCoffee()
+        {
+            DbContextOptions<CoffeeShopDbContext> options = new DbContextOptionsBuilder<CoffeeShopDbContext>().UseInMemoryDatabase("CanCreateCoffee").Options;
+            using (CoffeeShopDbContext context = new CoffeeShopDbContext(options))
+            {
+                Coffee coffee = new Coffee();
+                coffee.ID = 1;
+                coffee.Name = "latte";
+                coffee.Price = 2;
+                coffee.Description = "tasty";
+                coffee.URL = "www.coffee.com";
+
+                CoffeeManager coffeeManager = new CoffeeManager(context);
+
+                await coffeeManager.CreateCoffee(coffee);
+                await coffeeManager.DeleteCoffee(1);
+
+                var result = context.Coffee.Any(c => c.ID == 1);
+
+                Assert.False(result);
+            }
+        }
     }
-}
